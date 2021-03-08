a11caf9a59e8fa4688ba7b084efb10f8bad395c4
idrevnii <ssamozhen13@gmail.com>
2021-03-09 02:21:50 +0400

    Added marked feature

--- utility.js
+++ utility.js
@@ -8,7 +8,20 @@ function getArgument(message) {
     return message.slice(message.indexOf(' ') + 1);
 }
 
+async function isAdministrator(ctx, user) {
+    return ctx.getChatAdministrators().then((administrators) => {
+        return administrators.map((administrator) => {
+            if (!(administrator.user.is_bot)
+                && (administrator.status === 'administrator' || administrator.status === 'creator')
+                && administrator.user.id === user.id) {
+                return true;
+            }
+        }).filter(Boolean)[0];
+    })
+}
+
 module.exports = {
     getArgument,
-    getRandomNumber
+    getRandomNumber,
+    isAdministrator
 }
